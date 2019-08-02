a939b2a83b1e7a375aa103ed08ec29544829f0bd
odangogakiDeskTop <odagakidesktop@gmail.com>
2019-08-02 15:28:07 +0900

    bulma、font-awesomeのインストール

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+// vue-fontawesomeの設定
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+
+// vue-fontawesomeの設定
+library.add(faUserSecret)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
